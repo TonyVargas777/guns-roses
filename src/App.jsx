923d67156e051634df69772c94da2e80e7d53c08
Jose Antonio Vargas Garcia <tonacovargas@hotmail.com>
2024-12-14 13:46:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="logo">
+        <img src="./gnr.avif" alt="" />
+      </div>
+      <section>
+        <img src="./1.avif" alt="captain-america" />
+        <img src="./3.avif" alt="captain-marvel2" />
+        <img src="./8.avif" alt="falcon" />
+        <img src="./13.avif" alt="iron-man" />
+        <img src="./26.avif" alt="captain-america" />
+        <img src="./24.avif" alt="thor" />
+        <img src="./25.avif" alt="viuda-negra" />
+        <img src="./17.avif" alt="deadpool" />
+        <img src="./18.avif" alt="spiderman" />
+        <img src="./27.avif" alt="drstrange" />
+        <img src="./4.avif" alt="drstrange" />
+      </section>
+    </div>
+  );
+}
+
+export default App;
